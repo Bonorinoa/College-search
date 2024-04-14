55dbb9a10c4b981defecc5b6cf402515e5ad7104
Augusto Gonzalez Bonorino <augusto.gonzalez-bonorino@cgu.edu>
2024-04-13 17:15:54 -0700

    front end full draft 1

--- main.py
+++ main.py
@@ -3,10 +3,32 @@ from time import sleep
 
 from utils import load_llm
 
+# streamlit fragment functions
+@st.experimental_fragment
+def fragment_function(parsed_data):
+    # dropdown is populated with the parsed data
+    st.markdown("## Select the author-institution pair")
+    
+    user_choice = st.selectbox("Author-Institution Pair", parsed_data)
 
+    st.write(f"User choice: {user_choice}")
+
+    # button to submit the choice
+    if st.button("Submit"):
+        st.write("You have submitted the choice")
+        st.balloons()
+
+        # user_choice is used here to fetch the data from the database or API
+        ## We need information about the author from some public API
+        ## Institution information can be fetched from the database
+
+        st.write("Thank you for using the web app!")
 
 def main():
-    #### WEB APP Code ####
+    
+    # streamlit state variables
+    
+    
     st.title("Hello - 5C Hachathon")
     st.write(
         "This is a simple web app to demonstrate the deployment of a machine learning model using Streamlit."
@@ -47,6 +69,8 @@ def main():
             sleep(2)
             
             st.success("Authors' profiles parsed successfully!")
+            
+        ## CACHE THE RESULTS CREATED UP TO THIS POINT
         
         
         # create list of strings to display in dropdown
@@ -59,22 +83,7 @@ def main():
 
         st.markdown("---")
 
-        # dropdown is populated with the parsed data
-        st.markdown("## Select the author-institution pair")
-        user_choice = st.selectbox("Author-Institution Pair", parsed_data)
-
-        st.write(f"User choice: {user_choice}")
-
-        # button to submit the choice
-        if st.button("Submit"):
-            st.write("You have submitted the choice")
-            st.balloons()
-
-            # user_choice is used here to fetch the data from the database or API
-            ## We need information about the author from some public API
-            ## Institution information can be fetched from the database
-
-            st.write("Thank you for using the web app!")
+        fragment_function(parsed_data)
 
 
 if __name__ == "__main__":
